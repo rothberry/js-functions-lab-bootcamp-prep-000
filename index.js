ebafda123bc1d2556c1c32710fe2c6ad5be3f387
Philip Roth <phil.roth077@gmail.com>
2019-05-28 02:49:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+function happyHolidays() {
+  return string('Happy holidays!')
+
+}
+
+function happyHolidaysTo(string) {
+  return string('Happy holidays, ${name}')
+}
+
+function happyHolidayTo(string) {
+  return string('Happy ${holiday}, ${name}')
+}
+
+function holidayCountdown(string) {
+  return string('Its ${days} until ${holiday}')
+
+}
